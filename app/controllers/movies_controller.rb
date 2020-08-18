9203b6167a04ac87c0300e8ba0c6ed5763a47c7e
kou <ec2-user@ip-172-31-62-185.ec2.internal>
2020-08-18 01:54:25 +0000

    add css

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,7 +2,7 @@ class MoviesController < ApplicationController
   before_action :require_user_logged_in
   
   def index
-    @movies = Movie.where(category: params[:category], initial: params[:initial]).order('title').page(params[:page]).per(15)
+    @movies = Movie.where(category: params[:category], initial: params[:initial]).order(title: :asc).page(params[:page]).per(15)
     @count_movies = Movie.where(category: params[:category], initial: params[:initial])
     counts_from_movie(@count_movies)
   end
