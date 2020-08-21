eb19a4f46f0180dc8d9a3a28f09b023ed3f666c0
kou <ec2-user@ip-172-31-62-185.ec2.internal>
2020-08-21 03:40:26 +0000

    Modified

--- db/migrate/20200820165029_add_collation_to_hurigana_on_movies.rb
+++
@@ -1,5 +0,0 @@
-class AddCollationToHuriganaOnMovies < ActiveRecord::Migration[5.2]
-  def change
-     change_column :movies, :hurigana, :string, collation: 'utf8_bin'
-  end
-end
--- db/migrate/20200821032130_change_collation_on_movies.rb
+++
@@ -1,5 +0,0 @@
-class ChangeCollationOnMovies < ActiveRecord::Migration[5.2]
-  def change
-    change_column :movies, :hurigana, :string, collation: 'utf8_general_ci'
-  end
-end
