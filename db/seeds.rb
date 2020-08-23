8bc414864411e36fb55b17d2b289ad777c367c69
kou <ec2-user@ip-172-31-62-185.ec2.internal>
2020-08-23 03:45:09 +0000

    Modified and add animationdata sa-no

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,8 +11,9 @@ require "csv"
 (1..10).each do |number|
     User.create(name: 'test_user' + number.to_s, password: 'test' + number.to_s)
 end
+=end
 
-
+=begin
 #csvファイル読み込み  
 CSV.foreach('db/movies.csv') do |row|
     Movie.create(:category => row[0], :title => row[1], :company => row[2], :year => row[3], :synopsis => row[4], :image_path => row[5], :initial => row[6], :hurigana => row[7])
@@ -23,3 +24,18 @@ CSV.foreach('db/animation_ki_ko_gi_go.csv') do |row|
     Movie.create(:category => row[0], :title => row[1], :company => row[2], :year => row[3], :synopsis => row[4], :image_path => row[5], :initial => row[6], :hurigana => row[7])
 end
 =end
+
+
+CSV.foreach('db/animation_sa_so_za_zo.csv') do |row|
+    Movie.create(:category => row[0], :title => row[1], :company => row[2], :year => row[3], :synopsis => row[4], :image_path => row[5], :initial => row[6], :hurigana => row[7])
+end
+
+CSV.foreach('db/animation_ta_to_da_do.csv') do |row|
+    Movie.create(:category => row[0], :title => row[1], :company => row[2], :year => row[3], :synopsis => row[4], :image_path => row[5], :initial => row[6], :hurigana => row[7])
+end
+
+CSV.foreach('db/animation_na_no.csv') do |row|
+    Movie.create(:category => row[0], :title => row[1], :company => row[2], :year => row[3], :synopsis => row[4], :image_path => row[5], :initial => row[6], :hurigana => row[7])
+end
+
+
