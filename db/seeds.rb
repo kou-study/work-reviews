4f4061c06a7912b7bdaad92df9eb6f39abebc9fe
kou <ec2-user@ip-172-31-62-185.ec2.internal>
2020-08-24 05:19:02 +0000

    add animationdata and Modified

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,25 +7,26 @@ require "csv"
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-=begin
-(1..10).each do |number|
+(1..5).each do |number|
     User.create(name: 'test_user' + number.to_s, password: 'test' + number.to_s)
 end
-=end
 
-=begin
-#csvファイル読み込み  
-CSV.foreach('db/movies.csv') do |row|
+#csvファイル読み込み
+
+#アニメ作品
+CSV.foreach('db/animation_forget.csv') do |row|
+    Movie.create(:category => row[0], :title => row[1], :company => row[2], :year => row[3], :synopsis => row[4], :image_path => row[5], :initial => row[6], :hurigana => row[7])
+end
+
+CSV.foreach('db/animation_a_o.csv') do |row|
     Movie.create(:category => row[0], :title => row[1], :company => row[2], :year => row[3], :synopsis => row[4], :image_path => row[5], :initial => row[6], :hurigana => row[7])
 end
 
 
-CSV.foreach('db/animation_ki_ko_gi_go.csv') do |row|
+CSV.foreach('db/animation_ka_ko_ga_go.csv') do |row|
     Movie.create(:category => row[0], :title => row[1], :company => row[2], :year => row[3], :synopsis => row[4], :image_path => row[5], :initial => row[6], :hurigana => row[7])
 end
-=end
 
-=begin
 CSV.foreach('db/animation_sa_so_za_zo.csv') do |row|
     Movie.create(:category => row[0], :title => row[1], :company => row[2], :year => row[3], :synopsis => row[4], :image_path => row[5], :initial => row[6], :hurigana => row[7])
 end
@@ -37,9 +38,29 @@ end
 CSV.foreach('db/animation_na_no.csv') do |row|
     Movie.create(:category => row[0], :title => row[1], :company => row[2], :year => row[3], :synopsis => row[4], :image_path => row[5], :initial => row[6], :hurigana => row[7])
 end
-=end
 
-CSV.foreach('db/animation_ha_ho_ba_bo.csv') do |row|
+CSV.foreach('db/animation_ha_ho_ba_bo_pa_po.csv') do |row|
     Movie.create(:category => row[0], :title => row[1], :company => row[2], :year => row[3], :synopsis => row[4], :image_path => row[5], :initial => row[6], :hurigana => row[7])
 end
 
+
+CSV.foreach('db/animation_ma_mo.csv') do |row|
+    Movie.create(:category => row[0], :title => row[1], :company => row[2], :year => row[3], :synopsis => row[4], :image_path => row[5], :initial => row[6], :hurigana => row[7])
+end
+
+CSV.foreach('db/animation_ya_yo.csv') do |row|
+    Movie.create(:category => row[0], :title => row[1], :company => row[2], :year => row[3], :synopsis => row[4], :image_path => row[5], :initial => row[6], :hurigana => row[7])
+end
+
+CSV.foreach('db/animation_ra_ro.csv') do |row|
+    Movie.create(:category => row[0], :title => row[1], :company => row[2], :year => row[3], :synopsis => row[4], :image_path => row[5], :initial => row[6], :hurigana => row[7])
+end
+
+CSV.foreach('db/animation_wa.csv') do |row|
+    Movie.create(:category => row[0], :title => row[1], :company => row[2], :year => row[3], :synopsis => row[4], :image_path => row[5], :initial => row[6], :hurigana => row[7])
+end
+
+#映画作品
+CSV.foreach('db/movie.csv') do |row|
+    Movie.create(:category => row[0], :title => row[1], :company => row[2], :year => row[3], :synopsis => row[4], :image_path => row[5], :initial => row[6], :hurigana => row[7])
+end
